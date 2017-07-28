21a96c1796b03105b9db2e5174fe3ebab9a54a5c
Nikhil Sarvaiye <Nikhil.Sarvaiye@EYC.COM>
2017-07-29 03:11:55 +0530

    added post

--- webApp/src/app/modules/feed/feed.component.ts
+++ webApp/src/app/modules/feed/feed.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { IUserModel } from './../user/user.model';
 import { UserService } from './../user/user.service';
 import { DomController } from './../../shared/controllers/dom/dom-controller';
+import { IFeedModel } from './../../../backend/modules/feed/models/feed.model'
+import { FeedService } from './feed.service'
 
 @Component({
     selector: 'feed',
@@ -11,17 +13,48 @@ import { DomController } from './../../shared/controllers/dom/dom-controller';
 export class FeedComponent implements OnInit {
 
     private user: IUserModel;
-    
-    constructor(private userService: UserService) {
-        this.user = userService.getUserDetails();
+    private posts: IFeedModel[];
+    private newPost: IFeedModel;
+
+    constructor(private _userService: UserService, private _feedService: FeedService) {
+        this.user = _userService.getUserDetails();
+        this.newPost = <IFeedModel>{};
     }
-    
+
     public ngOnInit() {
         console.log('hello `Feed` component');
     }
 
     ngAfterViewInit() {
-        // DomController.profileCardLoaded();
+        
+    }
+
+    getFeeds() {
+        this._feedService.getUserFeeds(this.user.user_id).subscribe(feeds => {
+            this.posts = feeds;
+        });
+    }
+
+    post() {
+        debugger
+        if (this.newPost && this.newPost.text) {
+            this._feedService.postFeed(this.newPost).subscribe(post => {
+                this.posts.push(post);
+                this.newPost = <IFeedModel>{};
+            });
+        }
+    }
+
+    like(post: IFeedModel) {
+
+    }
+
+    loadComments(post: IFeedModel) {
+
+    }
+
+    comment(post: IFeedModel) {
+
     }
 }
 
--- webApp/src/app/modules/feed/feed.module.ts
+++ webApp/src/app/modules/feed/feed.module.ts
@@ -1,23 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }   from '@angular/forms';
 import { LayoutModule } from './../layout/layout.module';
 import { ProfileCardModule } from './../profile-card';
 import { ChatModule } from './../chat/chat.module';
 import { FeedComponent } from './feed.component';
+import { FeedService } from './feed.service';
 
 @NgModule({
   imports: [
     CommonModule,
     LayoutModule,
     ChatModule,
+    FormsModule,
     ProfileCardModule
   ],
   declarations: [
     FeedComponent
   ],
-  providers: [],
+  providers: [FeedService],
   exports: [
     FeedComponent
   ]
