e5966c9152780c83695cd4df9d61c32f9c1b1b57
Nikhil Sarvaiye <Nikhil.Sarvaiye@EYC.COM>
2017-07-29 07:00:16 +0530

    Added missing files for Feeds

---
+++ webApp/src/backend/modules/feed/models/feed.model.validation.ts
@@ -0,0 +1,9 @@
+import * as mongoose from "mongoose";
+import { HandleError, HandleErrorResponse, HandleErrorTypes } from './../../../core/error/error.handler'
+import { FeedConstants } from "./../feed.constants";
+import { IFeedModel } from "./feed.model";
+
+export class FeedValidation {
+
+    
+}
